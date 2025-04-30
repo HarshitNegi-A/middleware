1d3ae74067ca244040be78633b6bac929a59f215
HarshitNegi-A <harshitnegi226@gmail.com>
2025-04-30 19:02:48 +0530

    createOwnMiddleware

---
+++ index.js
@@ -0,0 +1,52 @@
+// const express=require('express')
+// const app=express()
+// let port=3200
+
+// //Authentication middleware
+// app.use((req,res,next)=>{
+//     console.log("Authentication middleware called")
+//     next()
+// })
+
+// app.use("/library-2",(req,res,next)=>{
+//     console.log("Book recommendations")
+//     next()
+// })
+
+// app.use("/library-3",(req,res,next)=>{
+//     console.log("Special accerss to research paper from proffessors and seniors")
+//     next();
+// })
+
+
+// app.get("/library-2",(req,res)=>{
+//     res.send("<h1>Library 2 entered</h1>")
+// })
+// app.get("/library-3",(req,res)=>{
+//     res.send("<h1>Library 3 entered</h1>")
+// })
+
+
+// app.listen(port,()=>{
+//     console.log("Server is running")
+// })
+
+const express = require('express');
+const app = express();
+const port = 3200;
+
+
+const addUser = (req, res, next) => {
+    req.user = "Guest";
+    next();
+};
+
+
+app.get('/welcome', addUser, (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
